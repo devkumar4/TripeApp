0d245a7ae57c39f58d070ebc4ccca033325c5fb3
devkumar4 <100475573+devkumar4@users.noreply.github.com>
2023-04-23 23:02:04 +0530

    Update firebaseConfig.js

--- Firebase/firebaseConfig.js
+++ Firebase/firebaseConfig.js
@@ -3,13 +3,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCEhtriMa4otiAOi_ETFK4veqEvdsbmXog",
-    authDomain: "foodapp-a5c65.firebaseapp.com",
-    projectId: "foodapp-a5c65",
-    storageBucket: "foodapp-a5c65.appspot.com",
-    messagingSenderId: "63578862922",
-    appId: "1:63578862922:web:e237c941352fe8cf52fdb3",
-    measurementId: "G-YB7RPWSWVZ"
+    /* ADD YOUR OWN FIREBASE API KEY */
 };
 
 if (!firebase.apps.length) {
